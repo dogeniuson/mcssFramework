abebab00fc191ac288a93ef2189610019de302e5
Jose Llanes <josega88@gmail.com>
2021-06-08 15:13:12 -0500

    framework added

---
+++ MCSS.xcframework/ios-arm64/MCSS.framework/Headers/MCSS.h
@@ -0,0 +1,18 @@
+//
+//  MCSS.h
+//  MCSS
+//
+//  Created by Jose Gabriel on 31/05/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MCSS.
+FOUNDATION_EXPORT double MCSSVersionNumber;
+
+//! Project version string for MCSS.
+FOUNDATION_EXPORT const unsigned char MCSSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCSS/PublicHeader.h>
+
+
