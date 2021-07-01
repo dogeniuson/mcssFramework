58aa2b36f124feb7e0e3bba6b68ea045d61601ac
Jose Llanes <josega88@gmail.com>
2021-07-01 16:32:38 -0500

    adding packcage.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+ import PackageDescription
+
+let package = Package(
+    name: "MCSS",
+    platforms: [
+      .iOS(.v14),
+      
+    ],
+    products: [
+        .library(name: "MCSS", targets: ["MCSS"])
+    ],
+    targets: [
+        .target(
+            name: "MCSS",
+            exclude: [
+                "Info.plist"
+            ],
+            resources: [
+               
+            ],
+            swiftSettings: [
+                .define("DECIDE", .when(platforms: [.iOS]))
+            ]
+        )
+    ]
+)
